d33aca37f04e5dd8f97255e6fb77c9219d07dcc2
jaskiratpal <jaskiratpal@mail.fresnostate.edu>
2020-03-03 22:23:37 -0500

    Sticky navbar and smooth transitions

--- client/src/Components/Global/Navbar/SideNavigation.js
+++ client/src/Components/Global/Navbar/SideNavigation.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { Menu, Icon } from "antd";
+import { Menu, Icon, Layout } from "antd";
 import { useHistory } from "react-router-dom";
 import { sideNavSize } from "../../Constants/constants";
 
 import "antd/dist/antd.css";
 import "./style.css";
+
+const { Sider, Content } = Layout;
 
 const SideNavigation = () => {
   const history = useHistory();
@@ -20,60 +22,58 @@ const SideNavigation = () => {
   };
 
   return (
-    <div
-      className="sideNavContainer"
+    <Layout
       style={{
-        "min-width": navCollapsed ? sideNavSize.collapsed : sideNavSize.expanded
+        maxWidth: navCollapsed ? sideNavSize.collapsed : sideNavSize.expanded,
+        minWidth: navCollapsed ? sideNavSize.collapsed : sideNavSize.expanded,
+        backgroundColor: "rgb(23, 23, 27)",
+        transition: `min-width 0.2s, max-width 0.2s`
       }}
     >
-      <Menu
-        className="antdMenu"
-        mode="inline"
-        theme="dark"
-        inlineCollapsed={navCollapsed}
-        selectable={false}
-      >
-        <Menu.Item
-          className="navCollapseToggler"
-          key="1"
-          onClick={toggleNavCollapsed}
-          title={"Expand"}
-        >
-          <Icon type={navCollapsed ? "double-right" : "double-left"} />
-          <span className="sideNavMenuLabel"> Collapse Menu </span>
-        </Menu.Item>
-        <Menu.Item
-          className="antdMenuItem"
-          key="2"
-          onClick={() => {
-            handleBuildClick("/");
-          }}
-        >
-          <Icon type="home" />
-          <span className="sideNavMenuLabel">Home</span>
-        </Menu.Item>
-        <Menu.Item
-          className="antdMenuItem"
-          key="3"
-          onClick={() => {
-            handleBuildClick("/builds/build/create");
-          }}
-        >
-          <Icon type="container" />
-          <span className="sideNavMenuLabel">Builds</span>
-        </Menu.Item>
-        <Menu.Item
-          className="antdMenuItem"
-          key="4"
-          onClick={() => {
-            handleBuildClick("/characters");
-          }}
-        >
-          <Icon type="user" />
-          <span className="sideNavMenuLabel">Characters</span>
-        </Menu.Item>
-      </Menu>
-    </div>
+      <Sider className="sideNavContainer" collapsed={navCollapsed}>
+        <Menu mode="inline" theme="dark" selectable={false}>
+          <Menu.Item
+            className="navCollapseToggler"
+            key="1"
+            onClick={toggleNavCollapsed}
+            title={"Expand"}
+          >
+            <Icon type={navCollapsed ? "double-right" : "double-left"} />
+            <span className="sideNavMenuLabel"> Collapse Menu </span>
+          </Menu.Item>
+          <Menu.Item
+            className="antdMenuItem"
+            key="2"
+            onClick={() => {
+              handleBuildClick("/");
+            }}
+          >
+            <Icon type="home" />
+            <span className="sideNavMenuLabel">Home</span>
+          </Menu.Item>
+          <Menu.Item
+            className="antdMenuItem"
+            key="3"
+            onClick={() => {
+              handleBuildClick("/builds/build/create");
+            }}
+          >
+            <Icon type="container" />
+            <span className="sideNavMenuLabel">Builds</span>
+          </Menu.Item>
+          <Menu.Item
+            className="antdMenuItem"
+            key="4"
+            onClick={() => {
+              handleBuildClick("/characters");
+            }}
+          >
+            <Icon type="user" />
+            <span className="sideNavMenuLabel">Characters</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+    </Layout>
   );
 };
 
