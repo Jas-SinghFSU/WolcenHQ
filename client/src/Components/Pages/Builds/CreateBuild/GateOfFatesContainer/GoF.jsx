2c0c52f6a35251e0f7d1e8cceb5b61b008e7d4fa
jaskiratpal <jaskiratpal@mail.fresnostate.edu>
2020-03-08 22:18:33 -0400

    Clean the data and handle node links

--- client/src/Components/Pages/Builds/CreateBuild/GateOfFatesContainer/GoF.jsx
+++ client/src/Components/Pages/Builds/CreateBuild/GateOfFatesContainer/GoF.jsx
@@ -8,9 +8,18 @@ const MARGIN = {
   RIGHT: 10
 };
 
+const ringInner = 350;
+const ringMiddle = 700;
+const ringOuter = 1044;
+let linkElems = require("../../../../../Data/GoF/nodeLinks.json");
+
+// rotation
+// style="transition: all 1s ease 0s; transform-origin: 522px 522px 0px; transform: rotate(120deg);"
+
 export default class GoF {
   constructor(element) {
     const init = async () => {
+      console.log("init");
       /* Create the SVG canvas */
       const svg = d3
         .select(element.current)
@@ -30,55 +39,141 @@ export default class GoF {
           return {};
         }
       };
-      console.log(JSON.stringify(svgData, null, 2));
+
+      let tooltip = d3
+        .select(element.current)
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("background", "yellow")
+        .style("color", "black")
+        .text("a simple tooltip");
+
+      const getAllNodes = () => {
+        let nodeMap = new Map();
+
+        svgData.svg.g.forEach(group => {
+          group.circle.forEach(circle => {
+            const { cx, cy, r } = circle;
+            nodeMap.set(circle.id, {
+              cx,
+              cy,
+              r,
+              class: circle.class
+            });
+          });
+        });
+
+        return nodeMap;
+      };
+
+      const allNodes = getAllNodes();
 
       const gElem = svg
         .selectAll("g")
         .data(svgData.svg.g)
         .join(
           enter => enter.append("g").attr("id", data => data.id),
-          // .selectAll("path")
-          // .data(data => data.path)
-          // .enter()
-          // .append("path")
-          // .attr("class", dataPath => dataPath.class)
-          // .attr("d", dataPath => dataPath.d)
-          // .attr("transform", dataPath => dataPath.transform)
-          // .data(data => data.line)
-          // .enter()
-          // .append("line")
-          // .attr("class", dataLine => {
-          //   console.log(dataLine);
-          //   return dataLine.class;
-          // })
-          // .attr("x1", dataLine => dataLine.x1)
-          // .attr("y1", dataLine => dataLine.y1)
-          // .attr("x2", dataLine => dataLine.x2)
-          // .attr("y2", dataLine => dataLine.y2),
           update => update.attr("class", "updated"),
           exist => exist.remove()
         );
 
-      const pathElem = gElem
-        .selectAll("path")
-        .data(data => data.path)
+      const wheelBG = gElem
+        .selectAll("image")
+        .data(data => data.scope)
         .enter()
-        .append("path")
-        .attr("class", dataPath => dataPath.class)
-        .attr("d", dataPath => dataPath.d)
-        .attr("transform", dataPath => dataPath.transform)
-        .data(data => data.line);
+        .append("svg:image")
+        .attr("xlink:href", data => {
+          if (data.current == "outerRing") {
+            return require("../../../../../images/Outer_Ring.png");
+          }
+          if (data.current == "innerRing") {
+            return require("../../../../../images/Inner_Ring.png");
+          }
+          if (data.current == "middleRing") {
+            return require("../../../../../images/Middle_Ring.png");
+          }
+        })
+        .attr("height", data => {
+          if (data.current == "outerRing") {
+            return ringOuter;
+          }
+          if (data.current == "innerRing") {
+            return ringInner;
+          }
+          if (data.current == "middleRing") {
+            return ringMiddle;
+          }
+        })
+        .attr("width", data => {
+          if (data.current == "outerRing") {
+            return ringOuter;
+          }
+          if (data.current == "innerRing") {
+            return ringInner;
+          }
+          if (data.current == "middleRing") {
+            return ringMiddle;
+          }
+        })
+        .attr("x", data => {
+          if (data.current == "outerRing") {
+            return (1044 - ringOuter) / 2;
+          }
+          if (data.current == "innerRing") {
+            return (1044 - ringInner) / 2;
+          }
+          if (data.current == "middleRing") {
+            return (1044 - ringMiddle) / 2;
+          }
+        })
+        .attr("y", data => {
+          if (data.current == "outerRing") {
+            return (1044 - ringOuter) / 2;
+          }
+          if (data.current == "innerRing") {
+            return (1044 - ringInner) / 2;
+          }
+          if (data.current == "middleRing") {
+            return (1044 - ringMiddle) / 2;
+          }
+        });
 
-      const lineElem = gElem
-        .selectAll("line")
-        .data(data => data.line)
+      // const lineElem = gElem
+      //   .selectAll("line")
+      //   .data(data => data.line)
+      //   .enter()
+      //   .append("line")
+      //   .attr("class", dataLine => dataLine.class)
+      //   .attr("x1", dataLine => dataLine.x1)
+      //   .attr("y1", dataLine => dataLine.y1)
+      //   .attr("x2", dataLine => dataLine.x2)
+      //   .attr("y2", dataLine => dataLine.y2);
+
+      const linkElem = gElem
+        .selectAll(null)
+        .data(linkElems)
         .enter()
         .append("line")
-        .attr("class", dataLine => dataLine.class)
-        .attr("x1", dataLine => dataLine.x1)
-        .attr("y1", dataLine => dataLine.y1)
-        .attr("x2", dataLine => dataLine.x2)
-        .attr("y2", dataLine => dataLine.y2);
+        .attr("class", dataLine => dataLine.isActive)
+        .attr("x1", dataLine => {
+          if (allNodes.get(dataLine.source))
+            return allNodes.get(dataLine.source).cx;
+        })
+        .attr("y1", dataLine => {
+          if (allNodes.get(dataLine.source))
+            return allNodes.get(dataLine.source).cy;
+        })
+        .attr("x2", dataLine => {
+          if (allNodes.get(dataLine.destination))
+            return allNodes.get(dataLine.destination).cx;
+        })
+        .attr("y2", dataLine => {
+          if (allNodes.get(dataLine.destination))
+            return allNodes.get(dataLine.destination).cy;
+        })
+        .style("stroke", "grey");
 
       const nodeElem = gElem
         .selectAll("circle")
@@ -95,12 +190,23 @@ export default class GoF {
           if (dataNode.id && dataNode.id.includes("-r")) {
             return "rgb(57, 0, 0)";
           } else if (dataNode.id && dataNode.id.includes("-g")) {
-            return "#350035";
-          } else if (dataNode.id && dataNode.id.includes("-p")) {
             return "#030";
+          } else if (dataNode.id && dataNode.id.includes("-p")) {
+            return "#350035";
           }
+        })
+        .on("mouseover", function(data) {
+          tooltip.text(data.id);
+          return tooltip.style("visibility", "visible");
+        })
+        .on("mousemove", function() {
+          return tooltip
+            .style("top", d3.event.pageY - 700 + "px")
+            .style("left", d3.event.pageX - 400 + "px");
+        })
+        .on("mouseout", function() {
+          return tooltip.style("visibility", "hidden");
         });
-      console.log(pathElem);
     };
 
     init();
