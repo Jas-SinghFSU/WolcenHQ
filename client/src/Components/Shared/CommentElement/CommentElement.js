6234a4b906bd1d23b4567195d6096880fd23501c
jaskiratpal <jaskiratpal@mail.fresnostate.edu>
2020-03-31 01:23:36 -0400

    Update comment component

--- client/src/Components/Shared/CommentElement/CommentElement.js
+++ client/src/Components/Shared/CommentElement/CommentElement.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { Avatar, Card } from "antd";
 
 import "./style.css";
 
 const CommentElement = () => {
   return (
     <div className="commentElementContainer">
-      <div className="commentAvatar">
-        <span>avatar</span>
+      <div className="commentAvatarContainer">
+        <Avatar
+          className="commentAvatar"
+          shape="square"
+          size="large"
+          icon="user"
+        />
       </div>
-      <div className="commentContent">
-        <span>content</span>
+      <div className="commentContentContainer">
+        <Card className="commentCard">
+          <div className="commentCardContent">
+            <div className="commentCardTop">
+              <span className="user">Guko</span>
+              <span className="date">Mar 31, 2020</span>
+            </div>
+            <div className="commentCardMiddle">
+              <span>Comment body.</span>
+            </div>
+            <div className="commentCardBottom">
+              <span className="controlsLeft">Like Dislike Reply</span>
+              <span className="controlsRight">Report</span>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
